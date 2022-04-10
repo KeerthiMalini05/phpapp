eb3b252539887fd5806b359034853dddcdb49b64
KeerthiMalini05 <58335834+KeerthiMalini05@users.noreply.github.com>
2022-04-10 14:54:07 +0530

    Update mysqlconnect.php

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -11,13 +11,13 @@ if($conn->connect_error) {
 }
 echo '--------- MySQL Connected successfully -------<br>';
 
-$sql1 = "CREATE TABLE IF NOT EXISTS persons( id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), city VARCHAR(50));";
+$sql1 = "CREATE TABLE IF NOT EXISTS books( id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), city VARCHAR(50));";
 if ($conn->query($sql1) === TRUE) {
-          echo "\nTable Persons created successfully <br>";
+          echo "\nTable Books created successfully <br>";
               
-              $sql = "insert into persons (name,city) values ('Adam','Coimbatore');";
-              //$sql .= "insert into persons (name,city) values ('Brevis','Chennai');";
-              //$sql .= "insert into persons (name,city) values ('Milne','Bangalore');";
+              $sql = "insert into books (name,author) values ('Alice in wonderland','chetan');";
+              $sql .= "insert into books (name,author) values ('Brevis-world','Maria');";
+              $sql .= "insert into books (name,author) values ('Milne-monarch','steven');";
               if ($conn->query($sql) === TRUE) {
               echo "\n New record created successfully <br>";
               } 
@@ -29,14 +29,14 @@ if ($conn->query($sql1) === TRUE) {
           echo "Error creating table: " . $conn->error;
 }
 
-echo 'List of Persons <br>';
-$sql = "SELECT * FROM persons;";
+echo 'List of Books with authors <br>';
+$sql = "SELECT * FROM books;";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
           // output data of each row
            while($row = $result->fetch_assoc()) {
-               echo " Name: " . $row["name"]. " City: " . $row["city"]. "<br>";
+               echo " Name: " . $row["name"]. " City: " . $row["author"]. "<br>";
                  }
             } else {
                    echo "0 results";
